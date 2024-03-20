ede54e401656bd435db38ec07b88331c6fb2f6de
gabriel <gabriel-lopes-universidade@proton.me>
2024-03-20 11:02:26 -0300

    20032024

--- media.c
+++ media.c
@@ -9,6 +9,12 @@ main() {
     scanf("%d", &v3);
     scanf("%d", &v4);
     scanf("%d", &v5);
-    media = (v1 + v2 + v3 + v4 + v5) /5;
+    /* primeira forma */
+    // media = (v1 + v2 + v3 + v4 + v5) / 5.0; // constante float
+    /* segunda forma */
+    media = v1 + v2 + v3 + v4 + v5;
+    media = media / 5;
+    /* terceira forma */
+    // media = (float) (v1 + v2 + v3 + v4 + v5) / 5;
     printf("a media dos valores e %f", media);
 }
---
+++ media2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main() {
+    int ler, ac;
+    float media;
+    printf("Digite os 5 valores: ");
+    scanf("%d", &ler);
+    ac = ac + ler;
+    scanf("%d", &ler);
+    ac = ac + ler;
+    scanf("%d", &ler);
+    ac = ac + ler;
+    scanf("%d", &ler);
+    ac = ac + ler;
+    scanf("%d", &ler);
+    ac = ac + ler;
+    media = (float) ac / 5;
+    printf("a media dos valores e %f", media);
+}
