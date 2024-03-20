c4a819c4265f76502508c367d03b23ba7d5dc32a
gabriel <gabriel-lopes-universidade@proton.me>
2024-03-20 12:28:05 -0300

    corrigir exercicios que deram ERRADO

--- exercicio_2.c
+++ exercicio_2.c
@@ -4,6 +4,5 @@ int main(){
     printf ("digite o numero de eleitores:\n");
     scanf ("%d", &eleitores);
     votos = (eleitores / 2) + 1;
-
     printf("Numero de votos necessarios no primeiro turno: %d\n", votos);
 }
