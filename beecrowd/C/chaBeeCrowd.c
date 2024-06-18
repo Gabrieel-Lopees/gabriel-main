524e36d600f8eb30b5b647c6b9b4f38ad1d1c899
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-06-18 11:49:01 -0300

    beeeeee

--- beecrowd/C/chaBeeCrowd.c
+++ beecrowd/C/chaBeeCrowd.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 
 main(){
-    int T;
-    int A, B, C, D;   
-    printf("Insira o tipo de chá: ");
-    scanf("%d", &T);
+    int T, acertos;
+    int A, B, C, D, E;
 
+    scanf("%d\n", &T);
+    scanf("%d" "%d" "%d" "%d" "%d", &A, &B, &C, &D, &E);
 
+    if (A == T) acertos++;
+    if (B == T) acertos++;
+    if (C == T) acertos++;
+    if (D == T) acertos++;
+    if (E == T) acertos++;
 
-
-
+    printf("%d\n", acertos);
 }
