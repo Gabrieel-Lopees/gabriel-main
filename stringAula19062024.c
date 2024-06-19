86a879aac86fc2d3d319509617988b21611bf366
Gabrieel-Lopees <gabriel-lopes-univeridade@proton.me>
2024-06-19 11:34:36 -0300

    aula de string

---
+++ stringAula19062024.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+    char nome[20];
+    int i=0;
+    printf("Digite o nome: ");
+
+    scanf("%s", nome);
+
+    // gets(nome);
+
+    for (i = 0; i < 20; i++)
+        printf("%c", nome[])
+
+
+
+
+
+}
