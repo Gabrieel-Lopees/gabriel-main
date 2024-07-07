14e04b3106338cf1d339be06bc86b9787cb6dc2e
Gabriel Lopes <gabriel-lopes-universidade@proton.me>
2024-07-06 22:45:41 -0300

    media beecrowd

---
+++ Ruby/Teste/untitled.rb
@@ -0,0 +1,10 @@
+# Lê as notas A, B e C do usuário
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+
+# Calcula a média ponderada
+media = (a * 2 + b * 3 + c * 5) / 10
+
+# Imprime a média com uma casa decimal
+puts("MEDIA =", media)
