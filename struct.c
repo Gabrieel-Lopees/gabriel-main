3235c7dd5b0ebcf03c4f6c16e1efded4694efb8d
Gabrieel-Lopees <gabriel.lopes@universidade@proton.me>
2024-07-09 11:04:01 -0300

    struct aula 09072024

---
+++ struct.c
@@ -0,0 +1,18 @@
+//struct 
+
+#include<stdio.h>
+
+int main(){
+
+    struct paciente{
+    char nome[30];
+    int dia, mes, ano;
+    float peso;
+    float altura;
+    };
+    
+struct paciente p, p1 ;
+
+
+
+}
