9df784898aa1797ba9b1312a3b0cb2537bf55c3c
Gabriel Lopes <gabriel-lopes-universidade@proton.me>
2024-09-25 14:30:35 -0300

    idai

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Gabriel Lopes</h1>
+<h3 align="center">Desenvolvedor e estudante da Universidade Federal de Santa Maria (UFSM)</h3>
+
+<img alt="my stats" align="Left" width="40%" src="https://github-readme-stats.vercel.app/api?username=Gabrieel-Lopees&show_icons=true&theme=graywhite&hide_border=false&rank_icon=github&include_all_commits=true"/>
+<img align="Right" src="https://github-readme-stats.vercel.app/api/top-langs?username=Gabrieel-Lopees&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=graywhite&hide_border=false&order=2" height="160" alt="languages graph"  />
+
+##
+
+<div align="Center">
+  <img src="https://github-readme-activity-graph.vercel.app/graph?username=Gabrieel-Lopees&radius=16&theme=minimal&area=true&order=5&hide_border=true&hide_title=false&line=636363%20&point=636363%20" height="275" alt="activity-graph graph"  />
+</div>
+
+##
+<img alt="badge" align="Left" src="https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white"/>
+<img alt="badge rb" align="Left" src="https://img.shields.io/badge/Ruby-%23CC0000.svg?style=for-the-badge&logo=ruby&logoColor=white"/>
+<img alt="badge python" align="Left" src="https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue"/>
+<img alt="badge tux" align="Left" src="https://img.shields.io/badge/Linux-000?style=for-the-badge&logo=linux&logoColor=FCC624"/>
+<img alt="badge" align="Left" src="https://img.shields.io/badge/Arch-1793D1?style=for-the-badge&logo=arch-linux&logoColor=white"/>
+<img alt="badge " align="Left" src="https://img.shields.io/badge/VIM-%2311AB00.svg?&style=for-the-badge&logo=vim&logoColor=white"/>
+<img alt="badge " align="Left" src="https://img.shields.io/badge/dell-XPS%208500%20-007DB8?style=for-the-badge&logo=dell&logoColor=white"/>
+
--- Segundo Semestre/Atividade 8.c
+++ Segundo Semestre/Atividade 8.c
@@ -1,17 +1,69 @@
 #include<stdio.h>
+#include <windows.h>
 
-int main()
-{
-    int a, r, an;
-    printf("Insira um elemento de progressão aritmetica: ");
-    scanf("%d", &a);
+int dinheiro = 0;
+int experiencia = 0;
+char dinheiroTexto[50], experienciaTexto[50];
 
-    printf("Insira a razão: ");
-    scanf("%d", &r);
+// Função para atualizar o texto dos labels
+void AtualizarLabels(HWND hWnd) {
+    sprintf(dinheiroTexto, "Dinheiro: %d", dinheiro);
+    sprintf(experienciaTexto, "Experiência: %d", experiencia);
+    SetWindowText(GetDlgItem(hWnd, 1), dinheiroTexto);
+    SetWindowText(GetDlgItem(hWnd, 2), experienciaTexto);
+}
 
-    printf("Os 10 elementos seguintes da PA são:\n");
-    for (int n = 1; n <=10; n++) {
-        an = a + n * r;
-        printf("%d\n", an);
+// Função callback da janela
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_COMMAND:
+            if (LOWORD(wParam) == 3) { // Botão de roubo
+                dinheiro += 1;
+                experiencia += 1;
+                AtualizarLabels(hWnd);
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Definir e registrar a classe da janela
+    const char CLASS_NAME[] = "SimuladorRoubador";
+
+    WNDCLASS wc = { };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
+
+    // Criar a janela
+    HWND hWnd = CreateWindowEx(0, CLASS_NAME, "Simulador de Roubador", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 300, 200,
+                               NULL, NULL, hInstance, NULL);
+    if (hWnd == NULL) {
+        return 0;
+    }
+
+    // Mostrar a janela
+    ShowWindow(hWnd, nCmdShow);
+
+    // Adicionar os elementos da interface
+    CreateWindow("STATIC", "Dinheiro: 0", WS_VISIBLE | WS_CHILD, 20, 20, 150, 20, hWnd, (HMENU) 1, hInstance, NULL);
+    CreateWindow("STATIC", "Experiência: 0", WS_VISIBLE | WS_CHILD, 20, 50, 150, 20, hWnd, (HMENU) 2, hInstance, NULL);
+    CreateWindow("BUTTON", "Roubar o troco da cantina", WS_VISIBLE | WS_CHILD, 20, 80, 200, 30, hWnd, (HMENU) 3, hInstance, NULL);
+
+    // Loop de mensagens
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
 }
