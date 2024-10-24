efc3a504d708c6d6c2f075d5fec3bcbf37ff805d
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-10-24 15:00:31 -0300

    24102024_2.cd 24102024_2.cm 24102024_2.cY

--- Segundo Semestre/24102024.c
+++ Segundo Semestre/24102024.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
  
-int main (){
-	for (int i = 10; i >= 1; i--) {
-	printf("Contagem: %d", i);	
+/* be quiet and drive */
+
+int main ()
+{
+
+int x, y;
+	printf("Insira o valor que será contado: ");
+		scanf("%d", &y);
+
+	printf("Insira até aonde ele será contado: ");
+		scanf("%d", &x);
+
+	for (int i = y; i <= x; i++) {
+		printf("Contagem: %d\n", i);	
 		}
+
+	return 0;
 }
+
