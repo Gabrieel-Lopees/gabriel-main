c765836bf34f41dd33736cc46db21ddb8d40c962
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-12-08 18:11:07 -0300

    08 12 2024 - Arch Linux Desktop

--- Segundo Semestre/Prova2/2.c
+++ Segundo Semestre/Prova2/2.c
@@ -13,6 +13,7 @@ int maior_valor(int vetor[], int tamanho) {
 int main() {
     int valores[5] = {15, 123, 3, 89, 21};
 
+
     int maior = maior_valor(valores, 5);
     printf("O maior valor do vetor é: %d\n", maior);
 
