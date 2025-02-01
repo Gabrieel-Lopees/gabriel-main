2c5600e724c85c55a3b44d3849fc674e2f753e56
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2025-02-01 20:58:52 -0300

    Commit Automatico do Vim - Arch Linux

---
+++ TerceiroSemestre/1deFevereiro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/* Beecrowdzudo */
+
+int main() {
+    int T;
+    int a, b, c, d, e;
+    scanf("%d", &T);
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("fim");
+
+}
