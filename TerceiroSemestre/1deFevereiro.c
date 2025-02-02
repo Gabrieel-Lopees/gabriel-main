8552387795875555ef6aad4483e65767a1cfc7ab
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2025-02-02 19:31:42 -0300

    Commit Automatico do Vim - Arch Linux

--- TerceiroSemestre/1deFevereiro.c
+++ TerceiroSemestre/1deFevereiro.c
@@ -3,7 +3,17 @@
 /* Beecrowdzudo */
 
 int main() {
-   
+   int time;
+   int speed;
+   float liters;
+   float distance;
 
+   scanf("%d", &time);
+   scanf("%d", &speed);
+
+   distance = speed * time;
+   liters = distance/12;
+
+   printf("%.3f", liters);
 
 }
