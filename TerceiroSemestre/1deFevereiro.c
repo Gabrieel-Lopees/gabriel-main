0841de47ca839d685218e8991a0ed3b2b8815def
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2025-02-02 19:23:24 -0300

    Commit Automatico do Vim - Arch Linux

--- TerceiroSemestre/1deFevereiro.c
+++ TerceiroSemestre/1deFevereiro.c
@@ -3,13 +3,7 @@
 /* Beecrowdzudo */
 
 int main() {
-   int distancia;
-   int minutos;
    
-   printf("insira a distancia: ");
-   scanf("%d", &distancia);
-   
-   minutos = distancia * 2;
 
-   printf("%d minutos", minutos);
+
 }
