dc8813c46d2f0bc914bc76752bba97098eaffa67
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2025-02-04 22:06:55 -0300

    Commit Automatico do Vim - Arch Linux

--- TerceiroSemestre/1deFevereiro.c
+++ TerceiroSemestre/1deFevereiro.c
@@ -3,15 +3,28 @@
 /* Beecrowdzudo */
 
 int main() {
-   int idadeDias;
-   int ano, mes, dia;
-   scanf("%d", &idadeDias);
+   int quantidade; 
+   float N;
 
-   ano = idadeDias / 365;
-   idadeDias %= 365;
-   mes = idadeDias / 30;
-   idadeDias %= 30;
+   float notas[] = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00};
+   float moedas[] = {1, 0.50, 0.25, 0.10, 0.05, 0.01};
+    
+   scanf("%f", &N);
+   
+
+   printf("NOTAS:\n");
+   for (int i = 0; i < 6; i++) {
+        quantidade = N / notas[i];
+        printf("%d nota(s) de R$ %.2f\n", quantidade, notas[i]);
+        N -= quantidade * notas[i];
+   }
+   
+    printf("MOEDAS:\n");
+    for (int i = 0; i < 6; i++) {
+        quantidade = N / moedas[i];
+        printf("%d moeda(s) de R$ %.2f\n", quantidade, moedas[i]);
+        N -= quantidade * moedas[i];
+    }
 
 
-   printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, idadeDias);
 }
